50ed3d11e106411829c82a4618df0dddcb8478e9
Carlos Martinez <curlosmart@gmail.com>
2021-10-30 16:58:56 -0400

    add char limit to tweet form

--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { AuthContext } from '../context/auth/AuthContext'
 import { TweetsContext } from '../context/tweets/TweetsContext'
+import TextareaAutosize from 'react-textarea-autosize';
 
 const FormTweet = styled.form`
   display: flex;
@@ -23,7 +24,7 @@ const FormCreate = styled.div`
   width: 100%;
 `
 
-const Textarea = styled.textarea`
+const FlexibleTextarea = styled(TextareaAutosize)`
   font-family: Roboto;
   font-size: 20px;
   color: white;
@@ -34,6 +35,7 @@ const Textarea = styled.textarea`
   padding: 10px;
   padding-top: 15px;
   padding-left: 15px;
+  white-space: pre-wrap;
 
   &:focus {
     outline: none;
@@ -72,19 +74,62 @@ const TweetButton = styled.button`
   padding: 12px;
   padding-left: 17px;
   padding-right: 17px;
-  width: 100%;
   cursor: pointer;
 `
 
+const TweetButtonAndCharLimit = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`
 
-const TweetForm = () => {
+const CharLimitCounter = styled.div`
+  color: ${props => props.charLimitReached ? 'red' : '#fff'};
+`
+
+
+const TweetForm = ({ replying }) => {
   
   const { user, userDispatch } = useContext(AuthContext)
   const { tweets, tweetsDispatch } = useContext(TweetsContext)
+  const MAX_CHAR_LIMIT = 400
 
   const [text, setText] = useState('')
 
+  const handleTextChange = (e) => {
+
+    if (e.target.value.length <= MAX_CHAR_LIMIT) {
+      setText(e.target.value)
+    }
+  }
+
   const handlePostTweet = async (e) => {
+    e.preventDefault()
+
+    if (!user) {
+      return
+    }
+
+    console.log(user)
+
+    try {
+      const body = {
+        userID: user._id,
+        text: text
+      }
+
+      console.log(body)
+      
+      const response = await axios.post('http://localhost:8888/api/tweets/tweet', body)
+      console.log('BOIS')
+      tweetsDispatch({ type: "UPDATE_TWEETS", payload: [...tweets, response.data]})
+      setText('')
+    } catch(err) {
+
+    }
+  }
+
+  const handleReplyTweet = async (e) => {
     e.preventDefault()
 
     if (!user) {
@@ -116,7 +161,7 @@ const TweetForm = () => {
       <UserIcon src="/images/user_icon.jpg"/>
 
       <FormCreate>
-        <Textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="What's happening?"/>
+        <FlexibleTextarea value={text} onChange={handleTextChange} placeholder="What's happening?"/>
 
         <FormActions>
 
@@ -127,12 +172,14 @@ const TweetForm = () => {
             <i class="far fa-image"></i>
           </MediaOptions>
 
-          <div>
-            <TweetButton onClick={handlePostTweet}>Tweet</TweetButton>
-          </div>
+          <TweetButtonAndCharLimit>
+            <CharLimitCounter charLimitReached={text.length === MAX_CHAR_LIMIT}>{text.length} / {MAX_CHAR_LIMIT}</CharLimitCounter>
+            {!replying ? (
+              <TweetButton onClick={handlePostTweet}>Tweet</TweetButton>
+            ) : <TweetButton onClick={handleReplyTweet}>Reply</TweetButton>}
+          </TweetButtonAndCharLimit>
         </FormActions>
       </FormCreate>
-      
     </FormTweet>
   );
 }
